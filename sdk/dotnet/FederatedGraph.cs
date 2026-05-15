693a8b6f1ffaf59ff04138e71c75a3560d22fc1d
Luigi Operoso <40476330+brokenpip3@users.noreply.github.com>
2026-05-15 11:50:17 +0200

    fix pluginDownloadURL (#6)

--- sdk/dotnet/FederatedGraph.cs
+++ sdk/dotnet/FederatedGraph.cs
@@ -77,7 +77,7 @@ namespace Pulumi.Cosmo
             var defaultOptions = new CustomResourceOptions
             {
                 Version = Utilities.Version,
-                PluginDownloadURL = "https://github.com/wundergraph/pulumi-cosmo/releases/download/v${VERSION}/",
+                PluginDownloadURL = "https://github.com/primait/pulumi-cosmo/releases/download/v${VERSION}/",
                 AdditionalSecretOutputs =
                 {
                     "admissionWebhookSecret",
